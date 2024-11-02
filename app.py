57bb5a1d6fbb8fc1e78aa6c0b731ada6307d2329
Renee1306 <108234614+Renee1306@users.noreply.github.com>
2024-11-02 19:17:20 +0800

    chg

--- app.py
+++ app.py
@@ -31,7 +31,8 @@ def preprocess_image(img_path, img_size):
     img = image.load_img(img_path, target_size=img_size)
     img_array = image.img_to_array(img)
     img_array = np.expand_dims(img_array, axis=0)  # Add batch dimension
-    return img_array  
+    img_array /= 255.0  # Normalize to [0, 1]
+    return img_array
 
 # Load environment variables from .env file
 load_dotenv()
