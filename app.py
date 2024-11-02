fd4a81be9451e92f388ab4bfd979ffa89b99fe89
Renee1306 <108234614+Renee1306@users.noreply.github.com>
2024-11-02 20:52:30 +0800

    s

--- app.py
+++ app.py
@@ -144,4 +144,4 @@ if __name__ == '__main__':
     # Ensure the temp directory exists
     if not os.path.exists('temp'):
         os.makedirs('temp')
-    app.run()
+    app.run(host='0.0.0.0', port=5000)
